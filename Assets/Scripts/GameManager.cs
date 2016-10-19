c180023aea82afea7d2c4b15f3f106cfecd1f903
bmgprakoso <bmgprakoso@gmail.com>
2016-10-19 13:44:55 +0700

    Initial Project

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	//Used for singleton
+	public static GameManager GM;
+
+	//Create Keycodes that will be associated with each of our commands.
+	//These can be accessed by any other script in our game
+	public KeyCode up {get; set;}
+	public KeyCode down {get; set;}
+	public KeyCode left {get; set;}
+	public KeyCode right {get; set;}
+
+
+
+	void Awake()
+	{
+		//Singleton pattern
+		if(GM == null)
+		{
+			DontDestroyOnLoad(gameObject);
+			GM = this;
+		}
+		else if(GM != this)
+		{
+			Destroy(gameObject);
+		}
+		/*Assign each keycode when the game starts.
+         * Loads data from PlayerPrefs so if a user quits the game,
+         * their bindings are loaded next time. Default values
+         * are assigned to each Keycode via the second parameter
+         * of the GetString() function
+         */
+		up = (KeyCode) System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("upKey", "W"));
+		down = (KeyCode) System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("downKey", "S"));
+		left = (KeyCode) System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("leftKey", "A"));
+		right = (KeyCode) System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("rightKey", "D"));
+
+	}
+
+	void Start ()
+	{
+
+	}
+
+	void Update ()
+	{
+
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;             //Floating point variable to store the player's movement speed.
+	private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+
+	// Use this for initialization
+	void Start()
+	{
+		//Get and store a reference to the Rigidbody2D component so that we can access it.
+		rb2d = GetComponent<Rigidbody2D> ();
+
+	}
+
+	//FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+	void FixedUpdate()
+	{
+		//float moveHorizontal = Input.GetAxis ("Horizontal");
+		//float moveVertical = Input.GetAxis ("Vertical");
+		//Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		//rb2d.AddForce (movement * speed);
+
+
+		/* Basic input logic. GetKey is grabbing
+
+         * the keycodes we created in our Game Manager
+
+         */
+
+		if (Input.GetKey (GameManager.GM.up)) {
+			transform.position += Vector3.up / 2;
+		}
+		if (Input.GetKey (GameManager.GM.down)) {
+			transform.position += Vector3.down / 2;
+		}
+		if (Input.GetKey (GameManager.GM.left)) {
+			transform.position += Vector3.left / 2;
+		}
+		if (Input.GetKey (GameManager.GM.right)) {
+			transform.position += Vector3.right / 2;
+		}
+
+			
+	}
+}
